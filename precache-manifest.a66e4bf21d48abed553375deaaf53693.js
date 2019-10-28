9d5c21012feaaa72d0f77cc7fc192a002515d7da
Isaac Hansen <isaac@Isaacs-MacBook-Pro-2.local>
2019-10-28 16:04:52 -0700

    Updates

---
+++ precache-manifest.a66e4bf21d48abed553375deaaf53693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cf45d8e8006d5064d569881fd82d82",
+    "url": "/QNDP-site/index.html"
+  },
+  {
+    "revision": "eb5b5cbb55cf5adb2d06",
+    "url": "/QNDP-site/static/css/main.0a8766e4.chunk.css"
+  },
+  {
+    "revision": "e107fc68357c4a2c5fe4",
+    "url": "/QNDP-site/static/js/2.b8c9765c.chunk.js"
+  },
+  {
+    "revision": "eb5b5cbb55cf5adb2d06",
+    "url": "/QNDP-site/static/js/main.42662d10.chunk.js"
+  },
+  {
+    "revision": "a4a07172c68a007d145a",
+    "url": "/QNDP-site/static/js/runtime-main.3306a6ba.js"
+  },
+  {
+    "revision": "6dbb6af8c15d60a1d5fea5d76d6cdbf9",
+    "url": "/QNDP-site/static/media/logo.6dbb6af8.png"
+  },
+  {
+    "revision": "9e1968ca19b7a39a96146c8aa5169898",
+    "url": "/QNDP-site/static/media/logo_coloredFT.9e1968ca.png"
+  }
+]);
